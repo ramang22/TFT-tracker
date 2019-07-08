3d4a785fc44b871ee4e76015b7bea7bdb266630c
Brach William <William.Brach@snt.sk>
2019-07-08 09:32:52 +0200

    changing project structure

---
+++ server/routes/users.js
@@ -0,0 +1,55 @@
+const _ = require('lodash')
+const lolApi = require('../API/riotGamesAPI')
+
+async function routes (fastify) {
+
+    fastify.get('/:summonerName', async (req, res) => {
+        const data = await lolApi.summoner([req.params.summonerName])
+        const summoner = {
+            id: data.id,
+            accId: data.accountId,
+            name: data.name
+            }
+
+        const matchHisotry = await lolApi.latest100games([summoner.accId])
+        const onlyADCgames = matchHisotry.matches.filter(el => el.lane === 'BOTTOM')
+        const first20games = _.slice(onlyADCgames, 0, 20)
+
+        let matchArray = []
+        for (let item of first20games) {
+            matchArray.push(item.gameId)
+        }
+        const matchData = await lolApi.getMatch(matchArray)
+
+        let participantID = 0
+        let wins = 0
+        let loses = 0
+
+        matchData.forEach((match) => {
+            match.participantIdentities.forEach((player) => {
+                if (player.player.summonerName === summoner.name) {
+                    participantID = player.participantId
+                }
+            })
+            match.participants.forEach((player) => {
+                if (player.participantId === participantID) {
+                    player.stats.win === true ? wins++ : loses++
+                }
+            })
+        })
+        let returnStat = {
+            win: wins,
+            lose: loses,
+            winrate: (100 / (wins + loses)) * wins,
+            name: summoner.name
+
+        }
+        res.send(returnStat)
+
+    })
+
+
+}
+
+
+module.exports = routes
---
+++ server/utils/secretkey.js
@@ -0,0 +1,2 @@
+const API_KEY = 'RGAPI-5679af28-27ea-4ec5-99f8-2270de8fac83'
+module.exports = { API_KEY }
