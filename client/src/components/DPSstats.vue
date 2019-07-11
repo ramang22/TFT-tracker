9cc89273f28a9938ede6eea7aebaaeb5b3ffff13
ramang22 <wibrach@gmail.com>
2019-07-11 21:47:46 +0200

    fixing

---
+++ client/src/components/DPSstats.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>DPS STATS FROM TFT</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+
+  },
+  methods: {
+  }
+};
+</script>
+<style scoped>
+</style>
+
