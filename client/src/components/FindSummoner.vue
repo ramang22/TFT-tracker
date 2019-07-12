af4acaaa7714c5b116eb75ec7832eeda4d4f74bf
ramang22 <wibrach@gmail.com>
2019-07-12 21:01:17 +0200

    layout update

--- client/src/components/FindSummoner.vue
+++ client/src/components/FindSummoner.vue
@@ -2,101 +2,95 @@
   <div>
     <div v-if="searchPressed === true">
       <div class="flex-container">
-        <div class="info">
-        <h3>Summoner : {{summoner.name}}</h3>
-        <vue-circle
-          :progress="summoner.winrate"
-          :size="150"
-          :reverse="false"
-          line-cap="round"
-          :fill="fill"
-          empty-fill="rgba(0, 0, 0, .1)"
-          :animation-start-value="0.0"
-          :start-angle="0"
-          insert-mode="append"
-          :thickness="5"
-          :show-percent="true"
-          @vue-circle-progress="progress"
-          @vue-circle-end="progress_end"
-        >
-          <p>Winrate</p>
-        </vue-circle>
-        <h3>Wins : {{summoner.win}}</h3>
-        <h3>Loses : {{summoner.lose}}</h3>
-      </div>
-      <div class="stats">
-        <h3>Player Stats</h3>
-        <table>
-          <tr>
-            <th>TOP 1</th>
-            <th>TOP 3</th>
-            <th>LOSE</th>
-          </tr>
-          <tr>
-            <td>
-              <vue-circle
-          :progress="20"
-          :size="80"
-          :reverse="false"
-          line-cap="round"
-          :fill="fill"
-          empty-fill="rgba(0, 0, 0, .1)"
-          :animation-start-value="0.0"
-          :start-angle="0"
-          insert-mode="append"
-          :thickness="5"
-          :show-percent="true"
-          @vue-circle-progress="progress"
-          @vue-circle-end="progress_end"
-        >
-          
-        </vue-circle>
-            </td>
-            <td>
-              <vue-circle
-          :progress="40"
-          :size="80"
-          :reverse="false"
-          line-cap="round"
-          :fill="fill"
-          empty-fill="rgba(0, 0, 0, .1)"
-          :animation-start-value="0.0"
-          :start-angle="0"
-          insert-mode="append"
-          :thickness="5"
-          :show-percent="true"
-          @vue-circle-progress="progress"
-          @vue-circle-end="progress_end"
-        >
-          
-        </vue-circle>
-            </td>
-            <td>
-              <vue-circle
-          :progress="60"
-          :size="80"
-          :reverse="false"
-          line-cap="round"
-          :fill="fill"
-          empty-fill="rgba(0, 0, 0, .1)"
-          :animation-start-value="0.0"
-          :start-angle="0"
-          insert-mode="append"
-          :thickness="5"
-          :show-percent="true"
-          @vue-circle-progress="progress"
-          @vue-circle-end="progress_end"
-        >
-          
-        </vue-circle>
-            </td>
-          </tr>
-        </table>
-        <h2>Match history</h2>
+        <div style="flex-grow: 2" class="info">
+          <h3>Summoner : {{summoner.name}}</h3>
+          <vue-circle
+            :progress="summoner.winrate"
+            :size="150"
+            :reverse="false"
+            line-cap="round"
+            :fill="fill"
+            empty-fill="rgba(0, 0, 0, .1)"
+            :animation-start-value="0.0"
+            :start-angle="0"
+            insert-mode="append"
+            :thickness="5"
+            :show-percent="true"
+            @vue-circle-progress="progress"
+            @vue-circle-end="progress_end"
+          >
+            <p>Winrate</p>
+          </vue-circle>
+          <h3>Wins : {{summoner.win}}</h3>
+          <h3>Loses : {{summoner.lose}}</h3>
+        </div>
+        <div style="flex-grow: 8" class="stats">
+          <h3>Player Stats</h3>
+          <table>
+            <tr>
+              <th>TOP 1</th>
+              <th>TOP 3</th>
+              <th>LOSE</th>
+            </tr>
+            <tr>
+              <td>
+                <vue-circle
+                  :progress="20"
+                  :size="80"
+                  :reverse="false"
+                  line-cap="round"
+                  :fill="fill"
+                  empty-fill="rgba(0, 0, 0, .1)"
+                  :animation-start-value="0.0"
+                  :start-angle="0"
+                  insert-mode="append"
+                  :thickness="5"
+                  :show-percent="true"
+                  @vue-circle-progress="progress"
+                  @vue-circle-end="progress_end"
+                ></vue-circle>
+              </td>
+              <td>
+                <vue-circle
+                  :progress="40"
+                  :size="80"
+                  :reverse="false"
+                  line-cap="round"
+                  :fill="fill"
+                  empty-fill="rgba(0, 0, 0, .1)"
+                  :animation-start-value="0.0"
+                  :start-angle="0"
+                  insert-mode="append"
+                  :thickness="5"
+                  :show-percent="true"
+                  @vue-circle-progress="progress"
+                  @vue-circle-end="progress_end"
+                ></vue-circle>
+              </td>
+              <td>
+                <vue-circle
+                  :progress="60"
+                  :size="80"
+                  :reverse="false"
+                  line-cap="round"
+                  :fill="fill"
+                  empty-fill="rgba(0, 0, 0, .1)"
+                  :animation-start-value="0.0"
+                  :start-angle="0"
+                  insert-mode="append"
+                  :thickness="5"
+                  :show-percent="true"
+                  @vue-circle-progress="progress"
+                  @vue-circle-end="progress_end"
+                ></vue-circle>
+              </td>
+            </tr>
+          </table>
+          <h2>Match history</h2>
+        </div>
       </div>
     </div>
-</div>
-      
+
     <div v-else>
       <img class="logo" src="../assets/tftlogo.jpeg" />
       <div>
@@ -117,7 +111,6 @@ export default {
       searchPressed: false,
       summoner: {},
       fill: { gradient: ["yellow"] }
-      
     };
   },
   components: {
@@ -146,13 +139,10 @@ export default {
 }
 .flex-container {
   display: flex;
-  
+  align-items: stretch;
 }
-.info {
-  padding-left : 5%;
-}
-.stats{
-  padding-left : 30%;
+table{
+  width: 100%;
 }
 </style>
 
